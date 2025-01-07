06d5775850d534ca81d00ca19877e35974bb4f36
JAgamez21 <zjulian1208@gmail.com>
2025-01-07 10:12:37 -0500

    feat: adicion excepciones

---
+++ src/main/java/com/example/banco_training_Julian_Agamez/exception/CuentaNoEncontradaException.java
@@ -0,0 +1,7 @@
+package com.example.banco_training_Julian_Agamez.exception;
+
+public class CuentaNoEncontradaException extends RuntimeException {
+    public CuentaNoEncontradaException(String mensaje) {
+        super(mensaje);
+    }
+}
---
+++ src/main/java/com/example/banco_training_Julian_Agamez/exception/UsuarioNoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.example.banco_training_Julian_Agamez.exception;
+
+public class UsuarioNoEncontradoException extends RuntimeException {
+    public UsuarioNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
+}

750a13baa46d4e5783436d26bb73d4c2136e25b3
JAgamez21 <zjulian1208@gmail.com>
2025-01-07 09:57:20 -0500

    feat: Inicializacion proyecto banco spring boot

---
+++ src/main/java/com/example/banco_training_Julian_Agamez/BancoTrainingJulianAgamezApplication.java
@@ -0,0 +1,13 @@
+package com.example.banco_training_Julian_Agamez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoTrainingJulianAgamezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoTrainingJulianAgamezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/banco_training_Julian_Agamez/BancoTrainingJulianAgamezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banco_training_Julian_Agamez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoTrainingJulianAgamezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
